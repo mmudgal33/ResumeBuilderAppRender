e3c5b1447e77b5024579f6b1212b55b1e47c5af4
mmudgal33 <mmudgal33@gmail.com>
2023-12-02 16:56:12 +0530

    commit deploy app

--- client/src/components/ResumeT.js
+++ client/src/components/ResumeT.js
@@ -118,8 +118,8 @@ const ResumeT = (props) => {
             {/* <button className="button" onClick={handleGeneratePdf}>
                 Generate PDF
             </button> */}
-            {/* <button className="button" onClick={downloadResume}> Generate PDF </button> */}
-            <button className="btn btn-info" onClick={downloadResume}> Generate PDF </button>
+            <button className="button" onClick={downloadResume}> Generate PDF </button>
+            {/* <button className="btn btn-info" onClick={downloadResume}> Generate PDF </button> */}
             {/* <button type="button" className="btn btn-info" onClick={downloadResume}> Generate PDF </button> */}
             {/* <div ref={reportTemplateRef}>  */}
             <div id ='download' > 
